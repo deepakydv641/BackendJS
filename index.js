e088dbf052e5fe4b40bf5666df6d7e2d8cedacde
deepakydv641 <deepakydv26422@gmail.com>
2025-09-07 12:35:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('deepak.com')
+})
+app.get('/login',(req,res)=>{
+    res.send('<h1>please login at Help for You</h1>')
+})
+app.get('/youtube',(req,res)=>{
+    res.send('<h2>Thanks for visiting here</h2>')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
